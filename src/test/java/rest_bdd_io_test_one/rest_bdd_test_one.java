e4007405d84a87e4c22abecc66c067a102bb6429
niteshmukati007@gmail.com <niteshmukati007@gmail.com>
2021-08-02 14:29:18 +0530

    first initial commit

---
+++ src/test/java/rest_bdd_io_test_one/rest_bdd_test_one.java
@@ -0,0 +1,56 @@
+package rest_bdd_io_test_one;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+
+
+import io.restassured.RestAssured;
+
+public class rest_bdd_test_one {
+	String body;
+
+	
+	@BeforeTest
+	public void baseUri()
+	{
+		RestAssured.baseURI="https://reqres.in/";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void test_01() 
+	{
+//		RestAssured.baseURI="https://reqres.in/";
+
+		given().
+				get("api/users/2").
+					then().body("data.id", equalTo(2)).log().all();
+	
+		
+		
+		
+		
+	}
+	
+	
+	@Test
+	public void test_02()
+	{	
+		System.out.println("This is also running!");	
+
+		
+		
+		
+	}
+
+}
