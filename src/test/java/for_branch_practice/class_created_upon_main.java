b37dfe960946dd013bc5fd2fb55373214150cfa4
niteshmukati007@gmail.com <niteshmukati007@gmail.com>
2021-08-02 15:08:28 +0530

    alpha branch

---
+++ src/test/java/for_branch_practice/class_created_upon_main.java
@@ -0,0 +1,16 @@
+package for_branch_practice;
+
+import org.testng.annotations.Test;
+
+public class class_created_upon_main {
+
+	@Test
+	public void test_01()
+	{
+		
+		System.out.println("This is the class i created on selecting the main branch to see and observe");
+	}
+	
+	
+	
+}
