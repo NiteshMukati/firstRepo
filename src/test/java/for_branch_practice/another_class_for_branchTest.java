2351a018cf758b9230903fbb93f275c023c76f68
niteshmukati007@gmail.com <niteshmukati007@gmail.com>
2021-08-02 14:58:49 +0530

    new class added

---
+++ src/test/java/for_branch_practice/another_class_for_branchTest.java
@@ -0,0 +1,13 @@
+package for_branch_practice;
+
+import org.testng.annotations.Test;
+
+public class another_class_for_branchTest {
+	
+	@Test
+	public void new_test_branch_method()
+	{
+		
+	}
+
+}
