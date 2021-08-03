a2115eaa4982f4f9ac94cdb1157fe63dbc37072f
niteshmukati007@gmail.com <niteshmukati007@gmail.com>
2021-08-03 11:17:24 +0530

    added beta features

---
+++ src/main/java/beta/branch/utils/betaUtilClass.java
@@ -0,0 +1,13 @@
+package beta.branch.utils;
+
+import org.testng.annotations.Test;
+
+public class betaUtilClass {
+	
+	@Test
+	public void utilclass()
+	{
+		System.out.println("This is the new test line we want to push into beta branch!");
+	}
+
+}
---
+++ src/test/java/beta/betaBranchTest01.java
@@ -0,0 +1,14 @@
+package beta;
+
+import org.testng.annotations.Test;
+
+public class betaBranchTest01 {
+	
+	
+	@Test
+	public void test01()
+	{
+		
+	}
+
+}
